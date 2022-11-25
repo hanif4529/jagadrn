904475d89f23618a88399d05b629c8908ad4f754
hanif4529 <ibrahim.hanif@adxasia.co.id>
2022-11-25 21:49:36 +0700

    refactor: pages and navigation

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import ChangePassword from "./ChangePassword";
+import ChangePasswordSucces from "./ChangePasswordSucces";
+import CreateNewPassword from "./CreateNewPassword";
+import OnBoarding from "./OnBoarding";
+import SecurityAccess from "./SecurityAccess";
+
+export { ChangePassword, ChangePasswordSucces, CreateNewPassword, OnBoarding, SecurityAccess };
--- router/index.js
+++ router/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import OnBoarding from "../pages/OnBoarding";
-import ChangePassword from "../pages/ChangePassword";
-import ChangePasswordSucces from "../pages/ChangePasswordSucces";
-import SecurityAccess from "../pages/SecurityAccess";
-import CreateNewPassword from "../pages/CreateNewPassword";
+import {OnBoarding, ChangePassword, CreateNewPassword, ChangePasswordSucces, SecurityAccess} from '../pages'
 
 const Stack = createNativeStackNavigator();
 
