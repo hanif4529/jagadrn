ab699bccbc249dc19c2a43a4a459260bb343f039
hanif4529 <ibrahim.hanif@adxasia.co.id>
2022-11-25 19:55:36 +0700

    style: adding custom font and splash screen

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textPop}>Hello Jagad Rn</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +35,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textPop: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 20,
   },
 });
